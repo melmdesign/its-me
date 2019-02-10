426c7c526af11479cbaf51186f7c5f0ad704843b
Liam Murphy <liamm.designs@gmail.com>
2019-02-10 12:26:26 +0100

    Various Tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
-  <div className="ProfileContainer">
-    <p className="DottedBox_content">Get started with CSS styling</p>
-  </div>
+    <div style={{ color: `black` }}>
+      <Header headerText="Liam Murphy, Lead Product Designer @Donuts" />
+      <Link to="work">Check my shit</Link>
+    </div>
 )
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,9 +1,12 @@
 import React from "react"
 import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="Works" /> 
+  <div style={{ color: `black` }}>
+    <Header headerText="Works" />
+    <Header headerText="and works and works" />
     <p>It's my work</p>
+    <Link to="/">Home</Link>
   </div>
 )
