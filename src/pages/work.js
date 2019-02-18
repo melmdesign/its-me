35a9f59641f532b6f74e1d5493ee89c2026726fb
Liam Murphy <liamm.designs@gmail.com>
2019-02-18 23:35:02 +0100

    Various Changes
    - Applied container to "Work" page
    - Added "company" component to "Work" page
    
    *Completed Lesson 2 of Gatsby Tutorial*

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,12 +1,35 @@
 import React from "react"
 import Header from "../components/header"
+import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
 import { Link } from "gatsby"
 
-export default () => (
-  <div style={{ color: `black` }}>
-    <Header headerText="Works" />
-    <Header headerText="and works and works" />
-    <p>It's my work</p>
-    <Link to="/">Home</Link>
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
   </div>
+)
+
+export default () => (
+  <Container>
+    <Link to="/">Home</Link>
+    <Header headerText="Resume or something?" />
+    <Header headerText="Where I've worked"
+    <User
+      username="Donut - trydonut.com"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="SumUp - sumup.com"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
 )
