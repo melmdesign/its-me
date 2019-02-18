c4bba379663d771e6bc234e8c1f852765b28b8ad
Liam Murphy <liamm.designs@gmail.com>
2019-02-18 23:38:55 +0100

    Update index.js
    Added header.js file reference to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Container from "../components/container"
+import Header from "../components/header"
 
 export default () => (
 <Container>
